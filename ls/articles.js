821d81ba60110612e4a8081e6cf856eeee355045
julianavalenti <julianabmartinez2@icloud.com>
2023-03-21 14:38:09 -0400

    adds automatic news on the home page

--- ls/articles.js
+++ ls/articles.js
@@ -32,7 +32,7 @@ function handleGetData(event) {
             li.append(a);
             li.appendChild(image)
             newsList.append(li);
-
+            $(".news-home").empty()
             console.log(newsList)
             
             
@@ -41,6 +41,38 @@ function handleGetData(event) {
          console.log('bad request', error); 
       };    
 }
+
+
+let home;
+const newsHome = document.querySelector('.news-home')
+const homeNews = document.createElement ('home')
+$.ajax({
+    url:'https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=6fa06be67aa24fc9be2a6483a327ebf9'
+   }).then(
+     (data) => {
+      console.log(data)
+      $(".news-home").empty()
+      data.articles.forEach(article => {
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        const image = document.createElement('span'); 
+        image.innerHTML = `<img src="${article.urlToImage}">`
+        a.href = article.url;
+        a.target = '_blank';
+        a.textContent = article.title;
+        li.append(a);
+        li.appendChild(image)
+        newsHome.append(li);
+
+        console.log(newsHome)
+      });
+
+
+
+    }),
+    (error) => {
+     console.log('bad request', error); 
+  };    
 
 
     
