45757f4edd48d7b753ef3554d8fbe6f8d706b00d
julianavalenti <julianabmartinez2@icloud.com>
2023-03-21 20:41:39 -0400

    adds script fixed

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 let news;
 const newsList = document.querySelector('#newsList')
 const newsImage = document.querySelector('.newsImage')
+const newsHome = document.querySelector('.news-home')
 const $input = $('input[type="text"]')
 let search = $input.val();
 
@@ -19,9 +20,9 @@ function handleGetData(event) {
        url:'https://newsapi.org/v2/everything?q=' + search + '&apiKey=6fa06be67aa24fc9be2a6483a327ebf9'
       }).then(
         (data) => {
-         console.log(data)
-            $(".newsList").empty()
-            data.articles.forEach(article => {
+          console.log(data)
+          $(".news-home").empty()
+          data.articles.forEach(article => {
             const li = document.createElement('li');
             const a = document.createElement('a');
             const image = document.createElement('span'); 
@@ -31,20 +32,22 @@ function handleGetData(event) {
             a.textContent = article.title;
             li.append(a);
             li.appendChild(image)
-            newsList.append(li);
-            $(".news-home").empty()
-            console.log(newsList)
-            
-            
-      })}),
+            newsHome.append(li);
+    
+            console.log(newsHome)
+          });
+    
+    
+    
+        }),
         (error) => {
          console.log('bad request', error); 
       };    
-}
-
+    
+    }
 
 let home;
-const newsHome = document.querySelector('.news-home')
+
 const homeNews = document.createElement ('home')
 $.ajax({
     url:'https://newsapi.org/v2/top-headlines?pageSize=6&country=us&category=technology&apiKey=6fa06be67aa24fc9be2a6483a327ebf9'
